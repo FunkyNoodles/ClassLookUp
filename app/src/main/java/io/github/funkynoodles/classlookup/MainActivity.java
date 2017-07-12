62eb2da1661373ca9e2d1a5f3d5434501fd3f296
Funky Noodles <nickasds@hotmail.com>
2017-07-12 00:34:55 -0500

    Added basic fragments

--- app/src/main/java/io/github/funkynoodles/classlookup/MainActivity.java
+++ app/src/main/java/io/github/funkynoodles/classlookup/MainActivity.java
@@ -1,13 +1,75 @@
 package io.github.funkynoodles.classlookup;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    public static List<Fragment> activeFragments = new ArrayList<>();
+
     @Override
+    @SuppressWarnings("ConstantConditions")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomBar bottomBar = (BottomBar)findViewById(R.id.bottomBar);
+        bottomBar.selectTabAtPosition(0);
+        bottomBar.setOnTabSelectListener((new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                FragmentManager fragmentManager = getFragmentManager();
+                Fragment fragment;
+                switch (tabId){
+                    case R.id.tabHome:
+                        getSupportActionBar().setTitle(getString(R.string.textHome));
+                        fragment = fragmentManager.findFragmentByTag("Home Fragment");
+                        hideAllOtherFragments("Home Fragment");
+                        if(fragment != null){
+                            fragmentManager.beginTransaction().show(fragment).commit();
+                        }else{
+                            HomeFragment homeFragment = HomeFragment.newInstance();
+                            fragmentManager.beginTransaction().add(R.id.contentContainer, homeFragment, "Home Fragment").commit();
+                            activeFragments.add(homeFragment);
+                        }
+                        break;
+
+                    case R.id.tabSettings:
+                        getSupportActionBar().setTitle(getString(R.string.textSettings));
+                        fragment = fragmentManager.findFragmentByTag("Settings Fragment");
+                        hideAllOtherFragments("Settings Fragment");
+                        if(fragment != null){
+                            fragmentManager.beginTransaction().show(fragment).commit();
+                        }else{
+                            SettingsFragment settingsFragment = SettingsFragment.newInstance();
+                            fragmentManager.beginTransaction().add(R.id.contentContainer, settingsFragment, "Settings Fragment").commit();
+                            activeFragments.add(settingsFragment);
+                        }
+                        break;
+
+                }
+            }
+        }));
+    }
+
+    /**
+     * Hide all other fragment except for the one with the given tag
+     * @param fragmentTag tag of the fragment want to show
+     */
+    public void hideAllOtherFragments(String fragmentTag){
+        for(Fragment fragment : activeFragments){
+            if(!fragment.getTag().equals(fragmentTag)){
+                getFragmentManager().beginTransaction().hide(fragment).commit();
+            }
+        }
     }
 }
---
+++ app/src/main/java/io/github/funkynoodles/classlookup/SettingsFragment.java
@@ -0,0 +1,29 @@
+package io.github.funkynoodles.classlookup;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SettingsFragment extends Fragment {
+
+    public static SettingsFragment newInstance() {
+        return new SettingsFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+}
