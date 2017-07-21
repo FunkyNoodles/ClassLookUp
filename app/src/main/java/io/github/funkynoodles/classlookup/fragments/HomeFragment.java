7112806c18d8e733c16cf59b3ebcca37087857be
FunkyNoodles <nickasds@hotmail.com>
2017-07-21 17:03:38 -0500

    Added search button

--- app/src/main/java/io/github/funkynoodles/classlookup/fragments/HomeFragment.java
+++ app/src/main/java/io/github/funkynoodles/classlookup/fragments/HomeFragment.java
@@ -11,11 +11,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.weiwangcn.betterspinner.library.BetterSpinner;
+
+import org.joda.time.DateTime;
 
 import java.io.File;
 import java.text.DecimalFormat;
@@ -28,6 +33,8 @@ import io.github.funkynoodles.classlookup.R;
 import io.github.funkynoodles.classlookup.adapters.BuildingNameAdapter;
 import io.github.funkynoodles.classlookup.adapters.TermSpinnerAdapter;
 import io.github.funkynoodles.classlookup.lookup.SearchIndex;
+import io.github.funkynoodles.classlookup.lookup.TermIndex;
+import io.github.funkynoodles.classlookup.models.Section;
 import io.github.funkynoodles.classlookup.tasks.LoadScheduleTask;
 
 public class HomeFragment extends Fragment {
@@ -36,14 +43,18 @@ public class HomeFragment extends Fragment {
     private BuildingNameAdapter buildingNameAdapter;
     private SearchIndex searchIndex;
     private String currentTermName;
-    private Calendar selecetedCalendar;
+    private Calendar selectedCalendar;
 
+    private BetterSpinner termSpinner;
+    private AutoCompleteTextView buildingNameAutoText;
+    private EditText roomNameEditText;
     private TextView dateTextView;
     private TextView timeTextView;
+    private Button searchButton;
 
     public HomeFragment() {
         searchIndex = new SearchIndex();
-        selecetedCalendar = Calendar.getInstance();
+        selectedCalendar = Calendar.getInstance();
     }
 
     public static HomeFragment newInstance() {
@@ -74,10 +85,10 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        final BetterSpinner termSpinner = (BetterSpinner) view.findViewById(R.id.termSpinner);
+        termSpinner = (BetterSpinner) view.findViewById(R.id.termSpinner);
         termSpinner.setAdapter(termAdapter);
 
-        final AutoCompleteTextView buildingNameAutoText = (AutoCompleteTextView) view.findViewById(R.id.buildingNameAutoText);
+        buildingNameAutoText = (AutoCompleteTextView) view.findViewById(R.id.buildingNameAutoText);
         buildingNameAutoText.setThreshold(1);
         buildingNameAutoText.setAdapter(buildingNameAdapter);
 
@@ -85,7 +96,7 @@ public class HomeFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String termName = (String) parent.getItemAtPosition(position);
-                if(termName.equals(currentTermName)){
+                if (termName.equals(currentTermName)) {
                     return;
                 }
                 new LoadScheduleTask(searchIndex, getActivity(), termName, buildingNameAdapter).execute();
@@ -93,16 +104,18 @@ public class HomeFragment extends Fragment {
             }
         });
 
-        dateTextView = (TextView)view.findViewById(R.id.dateText);
-        dateTextView.setText(getDisplayDateString(selecetedCalendar));
+        roomNameEditText = (EditText)view.findViewById(R.id.roomNameText);
+
+        dateTextView = (TextView) view.findViewById(R.id.dateText);
+        dateTextView.setText(getDisplayDateString(selectedCalendar));
         dateTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        selecetedCalendar.set(year, month, dayOfMonth);
-                        dateTextView.setText(getDisplayDateString(selecetedCalendar));
+                        selectedCalendar.set(year, month, dayOfMonth);
+                        dateTextView.setText(getDisplayDateString(selectedCalendar));
                     }
                 },
                         Calendar.getInstance().get(Calendar.YEAR), Calendar.getInstance().get(Calendar.MONTH),
@@ -111,31 +124,34 @@ public class HomeFragment extends Fragment {
             }
         });
 
-        timeTextView = (TextView)view.findViewById(R.id.timeText);
-        timeTextView.setText(getDisplayTimeString(selecetedCalendar));
+        timeTextView = (TextView) view.findViewById(R.id.timeText);
+        timeTextView.setText(getDisplayTimeString(selectedCalendar));
         timeTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                        selecetedCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
-                        selecetedCalendar.set(Calendar.MINUTE, minute);
-                        timeTextView.setText(getDisplayTimeString(selecetedCalendar));
+                        selectedCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                        selectedCalendar.set(Calendar.MINUTE, minute);
+                        timeTextView.setText(getDisplayTimeString(selectedCalendar));
                     }
                 }, Calendar.getInstance().get(Calendar.HOUR), Calendar.getInstance().get(Calendar.MINUTE), false);
                 timePickerDialog.show();
             }
         });
 
+        searchButton = (Button) view.findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new SearchButtonClickListener());
+
         return view;
     }
 
-    public void updateTermAdapter(){
+    public void updateTermAdapter() {
         termAdapter.updateContent();
     }
 
-    private String getDisplayDateString(Calendar c){
+    private String getDisplayDateString(Calendar c) {
         String displayDate = c.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.ENGLISH);
         displayDate += ". " + c.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.ENGLISH);
         displayDate += " " + c.get(Calendar.DAY_OF_MONTH);
@@ -143,11 +159,39 @@ public class HomeFragment extends Fragment {
         return displayDate;
     }
 
-    private String getDisplayTimeString(Calendar c){
+    private String getDisplayTimeString(Calendar c) {
         DecimalFormat formatter = new DecimalFormat("00");
         String displayTime = Integer.toString(c.get(Calendar.HOUR));
         displayTime += ":" + formatter.format(c.get(Calendar.MINUTE));
         displayTime += " " + c.getDisplayName(Calendar.AM_PM, Calendar.LONG, Locale.ENGLISH);
         return displayTime;
+    }
+
+    private class SearchButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            String termName = termSpinner.getText().toString();
+            if (searchIndex.termIndexMap.get(termName) == null) {
+                searchButtonError();
+                return;
+            }
+            TermIndex termIndex = searchIndex.termIndexMap.get(termName);
+            String buildingName = buildingNameAutoText.getText().toString();
+            String roomName = roomNameEditText.getText().toString();
+            DateTime dateTime = new DateTime(selectedCalendar.get(Calendar.YEAR),
+                    selectedCalendar.get(Calendar.MONTH),
+                    selectedCalendar.get(Calendar.DAY_OF_MONTH),
+                    selectedCalendar.get(Calendar.HOUR_OF_DAY),
+                    selectedCalendar.get(Calendar.MINUTE));
+            Section section = termIndex.get(buildingName, roomName, dateTime);
+            if(section == null){
+                searchButtonError();
+                return;
+            }
+        }
+    }
+
+    private void searchButtonError(){
+        Toast.makeText(getActivity(), getString(R.string.textSearchInvalid), Toast.LENGTH_LONG).show();
     }
 }
